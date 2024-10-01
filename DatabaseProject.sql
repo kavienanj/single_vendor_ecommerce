bee02a84475af7ebdfcf6ec6e6bcca6c8205f8b1
ashanuk <155507056+ashanuk@users.noreply.github.com>
2024-10-01 13:34:59 +0530

    new

---
+++ DatabaseProject.sql
@@ -0,0 +1,198 @@
+create schema DataBaseProject;
+use DataBaseProject;
+
+CREATE TABLE `Category` (
+  `category_id` INT AUTO_INCREMENT,
+  `category_name` VARCHAR(255) not null unique,
+  `description` VARCHAR(255),
+  PRIMARY KEY (`category_id`)
+);
+
+CREATE TABLE `ParentCategory_Match` (
+  `category_id` INT,
+  `parent_category_id` INT,
+  PRIMARY KEY (`category_id`, `parent_category_id`),
+  FOREIGN KEY (`category_id`) REFERENCES `Category`(`category_id`)
+  on update cascade
+  on delete cascade,
+  FOREIGN KEY (`parent_category_id`) REFERENCES `Category`(`category_id`)
+  ON update cascade
+  on delete restrict
+);
+
+CREATE TABLE `Warehouse` (
+  `warehouse_id` INT AUTO_INCREMENT,
+  `location` VARCHAR(255) not null,
+  `capacity` INT NOT null,
+  PRIMARY KEY (`warehouse_id`)
+);
+
+CREATE TABLE `Product` (
+  `product_id` INT AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT null,
+  `description` VARCHAR(255),
+  `sku` VARCHAR(255),
+  `weight` FLOAT,
+  -- `default_price` FLOAT,
+  `warehouse_id` INT,
+  `created_at` DATETIME not null,
+  `updated_at` DATETIME,
+  PRIMARY KEY (`product_id`),
+  FOREIGN KEY (`warehouse_id`) REFERENCES `Warehouse`(`warehouse_id`)
+  ON DELETE restrict
+  on update cascade
+);
+
+CREATE TABLE `Product_Category_Match` (
+  `product_id` INT,
+  `category_id` INT,
+  PRIMARY KEY (`product_id`, `category_id`),
+  FOREIGN KEY (`product_id`) REFERENCES `Product`(`product_id`)
+  on delete cascade
+  on update cascade,
+  FOREIGN KEY (`category_id`) REFERENCES `Category`(`category_id`)
+  on update cascade
+  on delete restrict
+);
+
+CREATE TABLE `Role` (
+  `role_id` INT AUTO_INCREMENT,
+  `role_name` ENUM("Admin", "User", "Guest") NOT null,
+  `description` VARCHAR(255),
+  PRIMARY KEY (`role_id`)
+);
+
+CREATE TABLE `User` (
+  `user_id` INT AUTO_INCREMENT,
+  `first_name` VARCHAR(255) not null,
+  `last_name` VARCHAR(255) not null,
+  `email` VARCHAR(255) not null unique,
+  `password_hash` VARCHAR(255) not null,
+  `phone_number` VARCHAR(255) not null unique,
+  `is_guest` BOOLEAN not null,
+  `role_id` INT not null,
+  `created_at` DATETIME not null,
+  `last_login` DATETIME,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`role_id`) REFERENCES `Role`(`role_id`)
+  ON DELETE restrict
+  on update cascade
+);
+
+CREATE TABLE `Cart` (
+  -- `cart_item_id` INT AUTO_INCREMENT,
+  `user_id` INT,
+  `variant_id` INT,
+  `quantity` INT,
+  -- PRIMARY KEY (`cart_item_id`),
+  PRIMARY KEY (`user_id`,`variant_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`)
+  on delete cascade
+  on update cascade,
+  FOREIGN KEY (`variant_id`) REFERENCES `Variant`(`variant_id`)
+  on delete cascade
+  on update cascade
+);
+
+CREATE TABLE `Transaction` (
+  `transaction_id` INT AUTO_INCREMENT,
+  `order_id` INT,
+  `status` ENUM("Completed", "Failed"),
+  `transaction_date` DATETIME,
+  `amount` FLOAT,
+  PRIMARY KEY (`transaction_id`),
+  FOREIGN KEY (`order_id`) REFERENCES `Order`(`order_id`)
+);
+
+CREATE TABLE `Order` (
+  `order_id` INT AUTO_INCREMENT,
+  `customer_id` INT,
+  `contact_email` VARCHAR(255),
+  `contact_phone` VARCHAR(255),
+  `delivery_method` ENUM("store_pickup", "delivery"),
+  `payment_method` ENUM("Cash_on_delivery", "card"),
+  `total_amount` FLOAT,
+  `order_status` ENUM("Processing", "Shipped", "Completed"),
+  `purchased_time` DATETIME,
+  PRIMARY KEY (`order_id`),
+  FOREIGN KEY (`customer_id`) REFERENCES `User`(`user_id`) 
+  on update restrict
+  ON delete restrict
+  
+);
+
+CREATE TABLE `Variant` (
+  `variant_id` INT AUTO_INCREMENT,
+  `product_id` INT,
+  `name` VARCHAR(255),
+  `price` FLOAT,
+  `created_at` DATETIME,
+  `updated_at` DATETIME,
+  PRIMARY KEY (`variant_id`),
+  FOREIGN KEY (`product_id`) REFERENCES `Product`(`product_id`)
+  on update cascade
+  on delete restrict
+);
+
+CREATE TABLE `OrderItem` (
+  `order_item_id` INT AUTO_INCREMENT,
+  `order_id` INT,
+  `variant_id` INT,
+  `discount` FLOAT,
+  `quantity` INT,
+  `price` FLOAT,
+  PRIMARY KEY (`order_item_id`),
+  FOREIGN KEY (`order_id`) REFERENCES `Order`(`order_id`),
+  FOREIGN KEY (`variant_id`) REFERENCES `Variant`(`variant_id`)
+);
+
+CREATE TABLE `Custom_Attribute` (
+  `attribute_id` INT AUTO_INCREMENT,
+  `product_id` INT,
+  `attribute_name` VARCHAR(255),
+  PRIMARY KEY (`attribute_id`),
+  FOREIGN KEY (`product_id`) REFERENCES `Product`(`product_id`)
+);
+
+CREATE TABLE `Custom_Attribute_Value` (
+  `variant_id` INT,
+  `attribute_id` INT,
+  `attribute_value` VARCHAR(255),
+  PRIMARY KEY (`variant_id`, `attribute_id`),
+  FOREIGN KEY (`variant_id`) REFERENCES `Variant`(`variant_id`),
+  FOREIGN KEY (`attribute_id`) REFERENCES `Custom_Attribute`(`attribute_id`)
+);
+
+CREATE TABLE `Inventory` (
+  `inventory_id` INT AUTO_INCREMENT,
+  `warehouse_id` INT,
+  `variant_id` INT,
+  `quantity_available` INT,
+  `last_updated` DATETIME,
+  PRIMARY KEY (`inventory_id`),
+  FOREIGN KEY (`warehouse_id`) REFERENCES `Warehouse`(`warehouse_id`)
+  on delete restrict
+  on update cascade,
+  FOREIGN KEY (`variant_id`) REFERENCES `Variant`(`variant_id`)
+  on delete restrict
+  on update cascade
+);
+
+DELIMITER $$
+
+CREATE PROCEDURE ADD_WAREHOUSE (location VARCHAR(255) , capacity INT)
+BEGIN
+INSERT INTO Warehouse VALUES (default,location,capacity);
+END$$
+
+CREATE PROCEDURE ADD_TO_CART (user_id INT , variant_d INT , quantity INT)
+BEGIN
+IF quantity IS NULL THEN
+	SET quantity = 1 ;
+END IF;
+INSERT INTO Cart VALUES (user_id,variant_id,quantity);
+END$$
+
+
+DELIMITER ;
+
